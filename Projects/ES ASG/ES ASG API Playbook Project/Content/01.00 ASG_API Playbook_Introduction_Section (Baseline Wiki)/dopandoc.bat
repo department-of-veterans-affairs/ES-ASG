64266502e4fe66f216b6510baee5e89ddf9990aa
esarks <ptm@esarks.com>
2018-07-31 20:39:28 -0400

    update

--- Projects/ES ASG/ES ASG API Playbook Project/Content/01.00 ASG_API Playbook_Introduction_Section (Baseline Wiki)/dopandoc.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/01.00 ASG_API Playbook_Introduction_Section (Baseline Wiki)/dopandoc.bat
@@ -1,2 +1,3 @@
-pandoc -f docx -t mediawiki -o "ASG_API Playbook_01.00 Introduction_Section_01.01_Initial Draft.mediawiki" "ASG_API Playbook_01.00 Introduction_Section_01.01_Initial Draft.docx"
+rem pandoc -f docx -t mediawiki -o "ASG_API Playbook_01.00 Introduction_Section_01.01_Initial Draft.mediawiki" "ASG_API Playbook_01.00 Introduction_Section_01.01_Initial Draft.docx"
+pandoc --from html --to mediawiki "ASG_API Playbook_01.00 Introduction_Section_01.01_Initial Draft - Copy.htm" > myfile.mediawiki
 pause
