e03e35a45c18a86c1fa7882ed80eb39569e27f5b
esarks <ptm@esarks.com>
2018-08-02 15:00:35 -0400

    Image

--- Projects/ES ASG/ES ASG API Playbook Project/Content/01.00 ASG_API Playbook_Introduction_Section/dopandoc.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/01.00 ASG_API Playbook_Introduction_Section/dopandoc.bat
@@ -1,3 +1,3 @@
 rem pandoc -f docx -t mediawiki -o "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.mediawiki" "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.docx"
-pandoc --extract-media ./images -t mediawiki -o "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.mediawiki" "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.docx"
+pandoc --extract-media ./ -t mediawiki -o "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.mediawiki" "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.docx"
 pause
