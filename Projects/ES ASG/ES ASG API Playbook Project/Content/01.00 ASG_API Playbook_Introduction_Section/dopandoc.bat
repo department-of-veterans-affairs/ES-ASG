54f12074d93063511c39cff8ad01f86c837cd032
esarks <ptm@esarks.com>
2018-08-02 22:24:19 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/01.00 ASG_API Playbook_Introduction_Section/dopandoc.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/01.00 ASG_API Playbook_Introduction_Section/dopandoc.bat
@@ -1,7 +1,6 @@
 rmdir media
 
-rem pandoc -f docx -t mediawiki -o "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.mediawiki" "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.docx"
-pandoc --extract-media ./ -t mediawiki -o "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.mediawiki" "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.docx"
+pandoc --extract-media ./ -t mediawiki -o "ASG_API Playbook_01.03 Introduction Section_01.05_Publication Review {Paul}.mediawiki" "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.docx"
 
 cd media
 for /R %%f in (*.emf) do (
