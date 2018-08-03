e34f1b98e9a2818f10ce86402c35e22a7012d7e7
esarks <ptm@esarks.com>
2018-08-03 13:31:48 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/12.03 ASG_API Playbook_Reuse Guidelines_Section/dopandoc.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/12.03 ASG_API Playbook_Reuse Guidelines_Section/dopandoc.bat
@@ -1,15 +1,24 @@
+set aImage="https://github.com/department-of-veterans-affairs/ES-ASG/blob/master/Projects/ES%20ASG/ES%20ASG%20API%20Playbook%20Project/Content/12.03%20ASG_API%20Playbook_Reuse%20Guidelines_Section/media/"
+
+rem get the last .docx file
 for /R %%f in (*.docx) do set aFile=%%~nf
 
+rem convert to docx to mediawiki and extract images
 pandoc --extract-media ./ -t mediawiki -o "%aFile%.mediawiki" "%aFile%.docx"
 
-for /R %%f in (*.docx) do (
-	uedit64 "%aFile%.mediawiki" /m,e="C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content\12.03 ASG_API Playbook_Reuse Guidelines_Section\fixURL.mac"
-)
+rem Fix up image URLs
+powershell -Command "(gc '%aFile%.mediawiki') -replace '.emf', '.png' | Out-File '%aFile%.mediawiki'"
+powershell -Command "(gc '%aFile%.mediawiki') -replace '.jpeg', '.png' | Out-File '%aFile%.mediawiki'"
+powershell -Command "(gc '%aFile%.mediawiki') -replace '.jpg', '.png' | Out-File '%aFile%.mediawiki'"
+powershell -Command "(gc '%aFile%.mediawiki') -replace '.gif', '.png' | Out-File '%aFile%.mediawiki'"
+powershell -Command "(gc '%aFile%.mediawiki') -replace '.tmp', '.png' | Out-File '%aFile%.mediawiki'"
+powershell -Command "(gc '%aFile%.mediawiki') -replace 'File:.//media/', '%aImage%' | Out-File '%aFile%.mediawiki'"
 
+rem housekeeping and move Wiki for publishing
 del *.bak
-
 copy "%aFile%.mediawiki" "C:\GitHub\ES-ASG.wiki"
 
+rem convert all image files to .png
 cd media
 for /R %%f in (*.emf) do (
 	magick %%~nf.emf %%~nf.png
@@ -30,6 +39,8 @@ for /R %%f in (*.tmp) do (
 for /R %%f in (*.gif) do (
 	magick %%~nf.gif %%~nf.png
 )
+
+rem push to GitHub Repo
 
 git add -f --all
 git commit -m "Publish"
---
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/copyit.bat
@@ -0,0 +1,2 @@
+for /r %%i in (.) do copy "C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content\dopandoc.bat" "%%i"
+pause
