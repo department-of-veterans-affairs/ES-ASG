5be9635e97da901ca6ba5eda2fec972073e15b28
esarks <ptm@esarks.com>
2018-08-02 22:21:43 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/01.00 ASG_API Playbook_Introduction_Section/dopandoc.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/01.00 ASG_API Playbook_Introduction_Section/dopandoc.bat
@@ -1,3 +1,26 @@
+rmdir media
+
 rem pandoc -f docx -t mediawiki -o "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.mediawiki" "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.docx"
 pandoc --extract-media ./ -t mediawiki -o "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.mediawiki" "ASG_API Playbook_01.03 Introduction Section_01.04_Publication Review {Paul}.docx"
+
+cd media
+for /R %%f in (*.emf) do (
+	magick %%~nf.emf %%~nf.png
+)
+
+cd media
+for /R %%f in (*.jpeg) do (
+	magick %%~nf.jpeg %%~nf.png
+)
+
+cd media
+for /R %%f in (*.jpg) do (
+	magick %%~nf.jpg %%~nf.png
+)
+
+cd media
+for /R %%f in (*.tmp) do (
+	magick %%~nf.tmp %%~nf.png
+)
+
 pause
