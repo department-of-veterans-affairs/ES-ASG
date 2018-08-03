b3bf97916057bb2e6159de75766b5f9fcf090faa
esarks <ptm@esarks.com>
2018-08-02 23:19:49 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/12.03 ASG_API Playbook_Reuse Guidelines_Section/dopandoc.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/12.03 ASG_API Playbook_Reuse Guidelines_Section/dopandoc.bat
@@ -7,19 +7,20 @@ for /R %%f in (*.emf) do (
 	magick %%~nf.emf %%~nf.png
 )
 
-cd media
 for /R %%f in (*.jpeg) do (
 	magick %%~nf.jpeg %%~nf.png
 )
 
-cd media
 for /R %%f in (*.jpg) do (
 	magick %%~nf.jpg %%~nf.png
 )
 
-cd media
 for /R %%f in (*.tmp) do (
 	magick %%~nf.tmp %%~nf.png
+)
+
+for /R %%f in (*.gif) do (
+	magick %%~nf.gif %%~nf.png
 )
 
 pause
