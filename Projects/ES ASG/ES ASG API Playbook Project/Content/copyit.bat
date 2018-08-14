c425ffde7217162c1b996d428bb5f6f354625e08
esarks <ptm@esarks.com>
2018-08-13 21:07:54 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/copyit.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/copyit.bat
@@ -1,5 +1,7 @@
 cd C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content
 
+echo [%date%, %time%] > TOC.txt
+
 for /r %%i in (.) do (
 	copy "C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content\dopandoc.bat" "%%i"
 	cd %%i
