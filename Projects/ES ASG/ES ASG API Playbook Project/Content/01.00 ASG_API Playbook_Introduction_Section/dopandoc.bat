125b68e4b9b873bc9a5c67e2d42a383636aef3cf
Paul Marshall <ptm@esarks.com>
2018-08-16 21:39:31 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/01.00 ASG_API Playbook_Introduction_Section/dopandoc.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/01.00 ASG_API Playbook_Introduction_Section/dopandoc.bat
@@ -78,9 +78,9 @@ rem Log to TOC
 cd "C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content"
 set aRoot=https://github.com/department-of-veterans-affairs/ES-ASG/wiki/
 set aURL=%aRoot%!aFile: =%%20!
-set aHref="~a href='%aURL%' target='_blank'~~%aFile%~/a~~"
-echo %aHref% > TOC.txt
-pause
+set aHref="<a href='%aURL%' target='_blank'>%aFile%</a>"
+echo %aHref% >> TOC.txt
+
 rem push to GitHub Repo
 
 cd "C:\GitHub\ES-ASG"
