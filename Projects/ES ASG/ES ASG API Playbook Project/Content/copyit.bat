c0293620f7bc2f8e331ebf625e15e65f394411de
Paul Marshall <ptm@esarks.com>
2018-08-18 22:24:42 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/copyit.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/copyit.bat
@@ -6,17 +6,12 @@ echo "<h3>Last Generated: [%date%, %time%]</h3>">TOC.txt
 echo "<ul>">>TOC.txt
 
 for /r %%i in (.) do (
-
-			if NOT %%i=="C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content\." (
 		    copy "C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content\dopandoc.bat" "%%i"
 		    cd %%i
 		    dopandoc
 		    cd ..
-      )
 )
-
+\
 cd "C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content"
 echo "</ul>">>TOC.txt
 
-rem Remove quotes
-powershell -Command "(gc 'TOC.txt' -encoding UTF8) -replace '"', '' | Out-File 'TOC.txt'" -encoding UTF8
