02a91a65a1e5a7df5f60b8d7fe3adda821ebe29f
Paul Marshall <ptm@esarks.com>
2018-08-19 08:35:53 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/copyit.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/copyit.bat
@@ -1,24 +1,23 @@
 cd C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content
 
-set aInit=2
+set aInit="1"
 
 echo "<h3>Last Generated: [%date%, %time%]</h3>">TOC.txt
 echo "<ul>">>TOC.txt
 
 for /r %%i in (.) do (
-pause
-			if %aInit%==2 (
-pause
+rem			if "%aInit%"=="2" (
 				set aInit=2
 		    copy "C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content\dopandoc.bat" "%%i"
 		    cd %%i
 		    dopandoc
 		    cd ..
-			) else (
-				set aInit=2
-			)
+rem			) else (
+rem				set aInit="2"
+rem				echo %aInit%
+rem			)
 )
-\
+
 cd "C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content"
 echo "</ul>">>TOC.txt
 
