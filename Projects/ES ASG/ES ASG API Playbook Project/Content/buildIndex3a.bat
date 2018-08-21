4e374a7f7dd31ff2935bf9c8ef1af61dee9dc13e
Paul Marshall <ptm@esarks.com>
2018-08-20 21:04:49 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/buildIndex3a.bat
+++
@@ -1,8 +0,0 @@
-SETLOCAL ENABLEDELAYEDEXPANSION
-
-for /R %%j in (*.docx) do (set aFile=%%~nj)	
-
-echo FILE "%aFile%">>"C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content\TOC.txt"
-
-cd "C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content"
-			
--- Projects/ES ASG/ES ASG API Playbook Project/Content/copyit0.bat
+++
@@ -1,5 +0,0 @@
-cd C:\GitHub\ES-ASG\Projects\ES ASG\ES ASG API Playbook Project\Content
-
-copyit1
-
-echo "</ul>">>TOC.txt
