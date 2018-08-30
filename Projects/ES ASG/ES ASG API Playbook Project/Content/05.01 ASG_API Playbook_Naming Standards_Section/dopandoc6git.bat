81d73de6d50e2dff99570458b0fee9121faa77a1
Paul Marshall <ptm@esarks.com>
2018-08-30 11:33:25 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/05.01 ASG_API Playbook_Naming Standards_Section/dopandoc6git.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/05.01 ASG_API Playbook_Naming Standards_Section/dopandoc6git.bat
@@ -57,9 +57,7 @@ rem Generated Timestamp
 powershell -Command "'<p>This page was generated from <b>' + '%aFile%' + '.docx</b> on <b>' + '%date%' + '</b> at <b>' + '%time%' + ' Eastern Time Zone</b>.</p>' + (13 -as [char]) + (10 -as [char]) + (gc '%aFolder%.mediawiki' -encoding UTF8 | Out-String) | Out-File '%aFolder%.mediawiki'" -encoding UTF8
 
 rem Fix up image URLs: replacing image types .emf, .jpeg, .jpg, .gif, .tmp with .png; replace File: URL with current section set as aImage above. Remove <blockquote> as it seems to cause problems
-powershell -Command "(gc '%aFolder%.mediawiki' -encoding UTF8) -replace '.emf', '.png' -replace '.jpeg', '.png' -replace '.jpg', '.png' -replace '.gif', '.png' -replace '.tmp', '.png' -replace 'File:.//media/', '%aImage%' -replace '<blockquote>', '' -replace '</blockquote>', '' -replace '&lt;pre&gt;', '<pre>' -replace '&lt;/pre&gt;', '</pre>' | Out-File '%aFolder%.mediawiki'" -encoding UTF8
-
-pause
+powershell -Command "(gc '%aFolder%.mediawiki' -encoding UTF8) -replace '.emf', '.png' -replace '.jpeg', '.png' -replace '.jpg', '.png' -replace '.gif', '.png' -replace '.tmp', '.png' -replace 'File:.//media/', '%aImage%' -replace '<blockquote>', '' -replace '</blockquote>', '' | Out-File '%aFolder%.mediawiki'" -encoding UTF8
 
 rem Move Wiki for publishing
 copy "%aFolder%.mediawiki" "C:\GitHub\ES-ASG.wiki"
