578542e6a6e9820ced61a0a107c66dc110e81298
Paul Marshall <ptm@esarks.com>
2018-08-30 09:56:32 -0400

    Publish

--- Projects/ES ASG/ES ASG API Playbook Project/Content/05.01 ASG_API Playbook_Naming Standards_Section/dopandoc6git.bat
+++ Projects/ES ASG/ES ASG API Playbook Project/Content/05.01 ASG_API Playbook_Naming Standards_Section/dopandoc6git.bat
@@ -57,11 +57,9 @@ rem Generated Timestamp
 powershell -Command "'<p>This page was generated from <b>' + '%aFile%' + '.docx</b> on <b>' + '%date%' + '</b> at <b>' + '%time%' + ' Eastern Time Zone</b>.</p>' + (13 -as [char]) + (10 -as [char]) + (gc '%aFolder%.mediawiki' -encoding UTF8 | Out-String) | Out-File '%aFolder%.mediawiki'" -encoding UTF8
 
 rem Fix up image URLs: replacing image types .emf, .jpeg, .jpg, .gif, .tmp with .png; replace File: URL with current section set as aImage above. Remove <blockquote> as it seems to cause problems
-rem powershell -Command "(gc '%aFolder%.mediawiki' -encoding UTF8) -replace '.emf', '.png' -replace '.jpeg', '.png' -replace '.jpg', '.png' -replace '.gif', '.png' -replace '.tmp', '.png' -replace 'File:.//media/', '%aImage%' -replace '<blockquote>', '' -replace '</blockquote>', '' -replace 'rel=' + (34 -as [char]) + 'nofollow' + (34 -as [char]) +'>', 'target=' + (34 -as [char]) +'blank' + (34 -as [char]) + '>' | Out-File '%aFolder%.mediawiki'" -encoding UTF8
-powershell -Command "(gc '%aFolder%.mediawiki' -encoding UTF8) -replace 'r', 'XXXXXXXXXXXXXXX' | Out-File '%aFolder%.mediawiki'" -encoding UTF8
+powershell -Command "(gc '%aFolder%.mediawiki' -encoding UTF8) -replace '.emf', '.png' -replace '.jpeg', '.png' -replace '.jpg', '.png' -replace '.gif', '.png' -replace '.tmp', '.png' -replace 'File:.//media/', '%aImage%' -replace '<blockquote>', '' -replace '</blockquote>', '' -replace 'rel=' + (34 -as [char]) + 'nofollow' + (34 -as [char]) +'>', 'target=' + (34 -as [char]) +'blank' + (34 -as [char]) + '>' | Out-File '%aFolder%.mediawiki'" -encoding UTF8
 
-
-
+rem powershell -Command "(gc '%aFolder%.mediawiki' -encoding UTF8) -replace 'r', 'XXXXXXXXXXXXXXX' | Out-File '%aFolder%.mediawiki'" -encoding UTF8
 rem <a href="http://www.ietf.org/rfc/rfc1035.txt" rel="nofollow">RFC 1035</a>
 
 
